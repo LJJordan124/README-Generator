99664fdb3243961ca095c534555113ec40d499ba
LJJordan124 <jordan3313.lj@gmail.com>
2023-07-17 19:29:13 -0400

    Update generateMarkdown.js
    Updating the function to generate markdown for README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,31 +13,31 @@ function generateMarkdown(data) {
   ${renderLicenseBadge(data.license)}
 
   ## Description
-${data.description}
-## Deployed Application URL
-${data.link}
-## Screenshot
-![alt-text](${data.screenshot})
-## Table of Contents
-* [Features](#features)
-* [Languages & Dependencies](#languagesanddependencies)
-* [How to Use This Application](#HowtoUseThisApplication)
-* [Contributors](#contributors)
-* [Testing](#testing)
-* [Questions](#questions)
-## Features
-${data.features}
-## Languages & Dependencies
-${data.require}
-## How to Use This Application:
-${data.usage}
-## Contributors
-${data.contributors}
-## Testing
-${data.test}
-## Questions
-  - [My GitHub Profile](https://github.com/${github})
-  - Email: ${email}
+  ${data.description}
+  ## Deployed Application URL
+  ${data.link}
+  ## Screenshot
+  ![alt-text](${data.screenshot})
+  ## Table of Contents
+  * [Features](#features)
+  * [Languages & Dependencies](#languagesanddependencies)
+  * [How to Use This Application](#HowtoUseThisApplication)
+  * [Contributors](#contributors)
+  * [Testing](#testing)
+  * [Questions](#questions)
+  ## Features
+  ${data.features}
+  ## Languages & Dependencies
+  ${data.require}
+  ## How to Use This Application:
+  ${data.usage}
+  ## Contributors
+  ${data.contributors}
+  ## Testing
+  ${data.test}
+  ## Questions
+    - [My GitHub Profile](https://github.com/${github})
+    - Email: ${email}
 
 `;
 }
